b26da05ea7e979a779463933dd1cd07a80c4b9a5
zpbappi <zpbappi@gmail.com>
2015-10-16 10:31:20 +1100

    changed the script to run asynchronously using Q

--- _scripts/generate.js
+++ _scripts/generate.js
@@ -1,20 +1,41 @@
 var yaml  = require('js-yaml'),
-    fs    = require('fs'),
+    q     = require('q'),
+    fs    = require('q-io/fs'),
     util  = require('util');
 
+// domain data
+var monthNames = {
+  "01": "January",
+  "02": "February",
+  "03": "March",
+  "04": "April",
+  "05": "May",
+  "06": "June",
+  "07": "July",
+  "08": "August",
+  "09": "September",
+  "10": "October",
+  "11": "November",
+  "12": "December"
+};
+
 // create tag files
-try {
-  var doc = yaml.safeLoad(fs.readFileSync(__dirname + '/../_data/tags.yml', 'utf8'));
+function createTagFiles() {
   var contentTemplate = "---\nlayout: tag\ntag: %s\npermalink: /tags/%s/\n---";
   var fileNameTemplate = __dirname + "/../tags/%s.md";
-  for(var key in doc){
-    var fileName = util.format(fileNameTemplate, key);
-    var content = util.format(contentTemplate, key, key);
-    if(!fs.existsSync(fileName))
-      fs.writeFileSync(fileName, content);
-  }
-} catch (e) {
-  console.log(e);
+  fs.read(__dirname + '/../_data/tags.yml', {'charset': 'utf8', 'flags': 'r'}).then(function(content){
+    return yaml.safeLoad(content);
+  }).then(function(doc){
+    var promises = Object.keys(doc).map(function(key){
+      var fileName = util.format(fileNameTemplate, key);
+      var content = util.format(contentTemplate, key, key);
+      return fs.exists(fileName).then(function(exists){
+        if(!exists)
+          fs.write(fileName, content);
+      });
+    });
+    return q.all(promises);
+  });
 }
 
 // create archive files
@@ -26,40 +47,47 @@ function getYearAndMonth(fileName){
   };
 };
 
-try{
-  var monthNames = {
-    "01": "January",
-    "02": "February",
-    "03": "March",
-    "04": "April",
-    "05": "May",
-    "06": "June",
-    "07": "July",
-    "08": "August",
-    "09": "September",
-    "10": "October",
-    "11": "November",
-    "12": "December"
-  };
-  var contentTemplate = "---\nlayout: archive\nyear: '%s'\nmonth: '%s'\nmonthName: %s\n---";
+function createSingleArchiveFile(monthDir, filePath, content){
+  return fs.makeTree(monthDir, "666").then(function(){
+    return fs.write(filePath, content);
+  });
+}
 
-  var files = fs.readdirSync(__dirname + '/../_posts/');
+function createArchiveFiles(){
+  var contentTemplate = "---\nlayout: archive\nyear: '%s'\nmonth: '%s'\nmonthName: %s\n---";
   var archiveDir = __dirname + '/../archive/';
-  for(var i=0; i<files.length; ++i){
-    var file = files[i];
-    var date = getYearAndMonth(file);
-    var yearDir = archiveDir + date.year.toString() + '/';
-    var monthDir = yearDir + date.month.toString() + '/';
-    if(!fs.existsSync(yearDir)){
-      fs.mkdirSync(yearDir);
+
+  fs.list(__dirname + '/../_posts/').then(function(files){
+    var data = {};
+    for(var i=0; i<files.length; ++i){
+      var file = files[i];
+      var date = getYearAndMonth(file);
+      if(!data[date.year]){
+        data[date.year] = [];
+      }
+      if(data[date.year].indexOf(date.month) === -1){
+        data[date.year].push(date.month);
+      }
     }
-    if(!fs.existsSync(monthDir)){
-      fs.mkdirSync(monthDir);
-      var content = util.format(contentTemplate, date.year, date.month, monthNames[date.month]);
-      fs.writeFileSync(archiveDir + date.year + '/' + date.month + "/index.html", content);
+    return data;
+  }).then(function(data){
+    var promises = [];
+    for(var year in data){
+      var yearDir = archiveDir + year.toString() + '/';
+      data[year].forEach(function(month){
+        var monthDir = yearDir + month.toString() + '/';
+        var content = util.format(contentTemplate, year, month, monthNames[date.month]);
+        var filePath = archiveDir + year + '/' + month + "/index.html";
+        promises.push(createSingleArchiveFile(monthDir, filePath, content));
+      });
     }
-  }
+    return q.all(promises);
+  });
 }
-catch(e){
-  console.log(e);
-}
+
+// main entry point
+q.all([createTagFiles(), createArchiveFiles()]).then(function(){
+  console.log("Tag and Archive files creation completed successfully.")
+}).fail(function(e){
+  console.error(e);
+});
