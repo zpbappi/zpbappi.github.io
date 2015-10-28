c7b2f50b3961f60e79d913c2b4cf6e6fa69ff519
zpbappi <zpbappi@gmail.com>
2015-10-28 13:38:02 +1100

    added pushing changes to github using GH_TOKEN

--- _scripts/main.js
+++ _scripts/main.js
@@ -1,12 +1,42 @@
 var fs = require("q-io/fs"),
     q = require("q"),
     git = require("gift-wrapper"),
-    generate = require("./generate.js");
+    generate = require("./generate.js"),
+    exec = require('child_process').exec;
 
-var repo = git(__dirname + "/../");
+var repoDir = __dirname + "/../"
+var credentialFilePath = repoDir + ".git/credentials";
+var repo = git(repoDir);
 
-function publishChanges(){
-  return repo.add("-A")
+function setAuthConfig(){
+  var deferred = q.defer();
+  exec("git config credential.helper store --file=" + credentialFilePath, 
+    {
+        cwd: repoDir,
+        encoding: "utf8",
+        maxBuffer: 5000 * 1024
+    },
+    function(err){
+      if(err){
+        return deferred.reject(err);
+      }
+      
+      return deferred.resolve();
+    }
+  );
+  
+  return deferred.promise;
+};
+
+function publishChanges(github_personal_access_token){
+  return setAuthConfig()
+  .then(function(){
+    var content = "https://" + github_personal_access_token + ":@github.com"
+    return fs.write(credentialFilePath, content);
+  })
+  .then(function(){
+    return repo.add("-A");
+  })  
   .then(function(){
     return repo.commit("[ci skip] automated tag and archive page generation");
   }).
@@ -15,13 +45,15 @@ function publishChanges(){
   });
 };
 
+
+// the main entry point
 generate()
 .then(function(){
   return repo.status();
 })
 .then(function(status){
   if(!status.clean){
-    return publishChanges();
+    return publishChanges(process.env.GH_TOKEN);
   }
 })
 .fail(console.error);
